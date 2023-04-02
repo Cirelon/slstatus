48eb514ce801fdeb4b91a6af693984d6d636ed5a
Cirelon <53874718+Cirelon@users.noreply.github.com>
2023-04-02 15:01:42 +0100

    Update config.h

--- config.h
+++ config.h
@@ -12,15 +12,15 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 
 	{ cpu_perc, 	"CPU %s %% ",	NULL },
-        { run_command,  "%s | ", 	"sensors | grep -A 0 'Tctl:' | cut -c16-22" },
+/*      { run_command,  "%s | ", 	"sensors | grep -A 0 'Tctl:' | cut -c16-22" },
 	{ run_command, 	"GPU %s ",  	"nvidia-smi -q -d UTILIZATION | grep Gpu | cut -c45-48" },
 	{ run_command, 	"%s°C ",   	"nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader --id=0" },
-	{ run_command, 	"%s | ",   	"nvidia-smi -q -d POWER | grep Draw | cut -c45-52" },
+	{ run_command, 	"%s | ",   	"nvidia-smi -q -d POWER | grep Draw | cut -c45-52" }, */
         { ram_used, 	"RAM %s | ", 	NULL },
 /*  	{ netspeed_rx, 	" ▼ %sB/s | ", 	"enp8s0" },
         { netspeed_tx, 	" ▲ %sB/s | ",  "enp8s0" }, */
-        { run_command,  "%s | ",        "cat /home/cirelon/.cache/torrent " },
-        { run_command, 	"%s | ",    	"bash ${XDG_CONFIG_HOME:-$HOME/.local}/bin/forecast" },
+/*      { run_command,  "%s | ",        "cat /home/cirelon/.cache/torrent " }, */
+        { run_command, 	"%s | ",    	"forecast" },
 	{ run_command, 	"📦 %s | ", 	"checkupdates | wc -l" },
         { run_command, 	"墳 %s | ",     "pamixer --get-volume-human" },
         { datetime, 	"%s", 		"%F - %T (%a)" },
